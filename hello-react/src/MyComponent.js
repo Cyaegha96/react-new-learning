da0106f14f48263eadecf231a5955a011f92bf9c
Cyaegha96 <zazae51@gmail.com>
2020-12-17 14:41:52 +0900

    react를 다루는 기술 3장까지

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,31 @@
-import React from 'react';
 
-const MyComponent = ()=>{
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
+import PropTypes from 'prop-types';
+
+import React, { Component } from 'react';
+
+class MyComponent extends Component{
+    render(){
+        const {name, favoriteNumber, children} = this.props;//비구조화 할당
+        return(
+            <div>
+                 안녕하세요, 제 이름은 {name}입니다. <br/>
+                 children 의 값은 {children}
+                 <br></br>
+                 제가 좋아하는 숫자는 {favoriteNumber}입니다.
+            </div>
+        )
+    }
 }
 
 export default MyComponent;
+
+
+MyComponent.defaultProps={
+    name:"기본 이름"
+};
+
+MyComponent.propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+}
+
