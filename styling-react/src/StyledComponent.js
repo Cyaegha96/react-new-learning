dd168be34dcd5d9e3270b09aa056601a58f0029b
Cyaegha96 <zazae51@gmail.com>
2020-12-24 15:06:47 +0900

    styled-component 반응형 @media 추가

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -6,6 +6,15 @@ const Box = styled.div`
     background: ${props => props.color || 'blue'};
     padding: 1rem;
     display: flex;
+
+    width: 1024px;
+    margin: 0 auto;
+    @media (max-width: 1024px){
+        width: 768px;
+    }
+    @media (max-width: 768px){
+        width: 100%;
+    }
 `;
 
 const Button = styled.button`
@@ -15,7 +24,7 @@ const Button = styled.button`
     padding: 0.5rem;
     display: flex;
     align-items: center;
-    jstify-content: center;
+    justify-content: center;
     box-sizing: border-box;
     font-size: 1rem;
     font-weight: 600;
