9a18d0226c5c60ac443208e4c34d1bc9d99fd6ed
Cyaegha96 <zazae51@gmail.com>
2020-12-24 14:25:34 +0900

    styling-react: CSSModule with Sass

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
 
 const App = () => {
   return (
     <div>
       렌더링 잘 되는거 맞음?
-      <SassComponent/>
+      <CSSModule/>
     </div>
   );
 };
