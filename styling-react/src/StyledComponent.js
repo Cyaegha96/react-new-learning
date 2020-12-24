f2ce6d82c0e2429098357e722b6e0098967f87f1
Cyaegha96 <zazae51@gmail.com>
2020-12-24 15:13:55 +0900

    media를 선언해줌으로써 편해짐

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -2,6 +2,22 @@ import styled, {css} from 'styled-components'
 
 import React from 'react';
 
+const sizes = {
+    desktop: 1024,
+    tablet: 768
+};
+
+//위에 있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어준다.
+const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em){
+        ${css(...args)};
+    }
+    `;
+    return acc;
+}, {});
+
+
 const Box = styled.div`
     background: ${props => props.color || 'blue'};
     padding: 1rem;
@@ -9,12 +25,18 @@ const Box = styled.div`
 
     width: 1024px;
     margin: 0 auto;
+    /*
     @media (max-width: 1024px){
         width: 768px;
-    }
+    } 
+    */
+    ${media.desktop`width:768px;`}
+    /* 
     @media (max-width: 768px){
         width: 100%;
     }
+    */
+    ${media.tablet`width:100%`}
 `;
 
 const Button = styled.button`
